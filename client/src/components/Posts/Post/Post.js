f713771b3b0140f6ed4de6d91d96c4cfb97c3f26
Junkim97 <53490816+Junkim97@users.noreply.github.com>
2021-03-31 11:36:36 -0700

    added title to my card overlay

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -17,6 +17,7 @@ const Post = ({post}) => {
       <div className={classes.overlay}>
         <Typography variant='h6'>{post.creator}</Typography>
         <Typography variant='body2'>{moment(post.createdAt).fromNow()}</Typography>
+        <Typography variant='h2'>{post.title}</Typography>
 
       </div>
 
