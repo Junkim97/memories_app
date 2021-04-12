9ce23cbdfc10659e8ce129774b49a93e3da0a1b4
Junkim97 <53490816+Junkim97@users.noreply.github.com>
2021-04-12 12:13:29 -0700

    started implementing authentication

---
+++ client/src/components/NavBar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {AppBar, Typography} from '@material-ui/core';
+import useStyles from './styles';
+
+const NavBar = () => {
+  const classes = useStyles();
+
+  return (
+    <AppBar className={classes.appBar} position='static' color='inherit'>
+        <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
+        <img className={classes.image} src={memories} alt="memories" height='60'></img>
+      </AppBar>
+  )
+}
+
+export default NavBar
