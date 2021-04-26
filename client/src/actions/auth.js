d159ba1d013615b5ef76a90f9af2f900a496c89a
Junkim97 <53490816+Junkim97@users.noreply.github.com>
2021-04-26 12:24:51 -0700

    finished up the backend for signin and signup and made users storable

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,15 +1,24 @@
 import {AUTH} from '../constants/actionTypes';
 import * as api from '../api';
 
-export const signup= () => async (dispatch) => {
+export const signup= (formData, history) => async (dispatch) => {
+  try {
+      // sign up the user
+    
+      history.push('/')
+  } catch (error) {
+    console.log(error)
+  }
 
 }
 
-export const signin= (formData) => async (dispatch) => {
+export const signin= (formData, history) => async (dispatch) => {
   try {
+      // log in the user
     
+      history.push('/')
   } catch (error) {
-    
+    console.log(error)
   }
 
 }
