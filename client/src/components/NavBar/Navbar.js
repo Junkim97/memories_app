c7c498ec4ff679d6b249253c35bc852755d03b3e
Junkim97 <53490816+Junkim97@users.noreply.github.com>
2021-05-17 12:25:55 -0700

    Finished up the middlewares and a check for expired tokens.

--- client/src/components/NavBar/Navbar.js
+++ client/src/components/NavBar/Navbar.js
@@ -2,6 +2,7 @@ import {React, useState, useEffect} from 'react';
 import {Link, useHistory, useLocation} from 'react-router-dom'
 import {AppBar, Button, Typography, Toolbar, Avatar} from '@material-ui/core';
 import {useDispatch} from 'react-redux';
+import decode from 'jwt-decode';
 import useStyles from './styles';
 import memories from '../../images/memories.png';
 
@@ -22,6 +23,14 @@ const Navbar = () => {
 
   useEffect(() => {
     const token = user?.token;
+
+    if(token) {
+      const decodedToken = decode(token);
+
+      if(decodedToken.exp * 1000 < new Date().getTime()) {
+        logout();
+      }
+    }
 
     setUser(JSON.parse(localStorage.getItem('profile')))
   }, [location]);
